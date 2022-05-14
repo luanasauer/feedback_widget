594114bfdf074a29496455536352d76364cd0803
Luana Sauer <luanasauer23@gmail.com>
2022-05-13 22:41:08 -0300

    ajustes para deploy

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express'; 
+import express from 'express';
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
@@ -9,19 +9,27 @@ routes.post('/feedbacks', async (req, res) => {
 
     const { type, comment, screenshot } = req.body;
 
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-    const nodemailerMailAdapter = new NodemailerMailAdapter();
+    try {
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+        const nodemailerMailAdapter = new NodemailerMailAdapter();
 
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbacksRepository,
-        nodemailerMailAdapter
-    );
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository,
+            nodemailerMailAdapter
+        );
 
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot
-    })
-    return res.status(201).send();
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot
+        })
+        return res.status(201).send();
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send();
+
+    }
+
 });
 
